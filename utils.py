2a27ba844f26bc7465e448ee23bb82ad1fabc602
Sameer-09816 <sameerkom16@outlook.com>
2025-09-14 11:18:17 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ import time
 import asyncio
 from bs4 import BeautifulSoup
 
-def generate_random_credentials():
+def generate_random_credentials(): # ... (code is unchanged)
     domains = ["vwh.sh", "iusearch.lol", "barid.site", "z44d.pro", "wael.fun", "kuruptd.ink"]
     random_part = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
     timestamp_part = int(time.time() * 1000)
@@ -16,7 +16,7 @@ def generate_random_credentials():
     last_name = ''.join(random.choices(string.ascii_lowercase, k=6))
     return {"email": email, "password": password, "firstName": first_name, "lastName": last_name}
 
-def extract_verification_link_from_html(html_content: str):
+def extract_verification_link_from_html(html_content: str): # ... (code is unchanged)
     try:
         soup = BeautifulSoup(html_content, 'html.parser')
         verify_link_tag = soup.find('a', string=lambda text: text and 'verify my email' in text.lower())
